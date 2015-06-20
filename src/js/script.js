1e5088787acfc1a90698480f7c89170549b6df1e
tsergeytovarov <tovarov.piter@gmail.com>
2015-06-20 19:03:50 +0400

    Хедер для незалогиненного пользователя без каталога

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,23 @@
-(function(){
+$(function(){
   console.log("let`s work hard!");
 
   // toggle tooltip
   $(".js-tooltip-toggle").on("click", function(event){
     event.preventDefault();
     $(this).parents(".tooltip-container").toggleClass("tooltip-container--active");
+  })
+
+  // js-open-header-find
+  $(".js-open-header-find").on("click", function(event){
+    event.preventDefault();
+    $(".main-header__left--basic").removeClass("main-header__left--open");
+    $(".main-header__left--find").addClass("main-header__left--open");
+  })
+
+  // js-close-header-find
+  $(".js-close-header-find").on("click", function(event){
+    event.preventDefault();
+    $(".main-header__left--basic").addClass("main-header__left--open");
+    $(".main-header__left--find").removeClass("main-header__left--open");
   })
 })
