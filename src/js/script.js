ef4d84ed456ce1e56910a2683ccc5fd96854f342
tsergeytovarov <tovarov.piter@gmail.com>
2015-06-20 13:56:20 +0400

    Главный экран с тултипом на кнопке вход

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,9 @@
 (function(){
   console.log("let`s work hard!");
+
+  // toggle tooltip
+  $(".js-tooltip-toggle").on("click", function(event){
+    event.preventDefault();
+    $(this).parents(".tooltip-container").toggleClass("tooltip-container--active");
+  })
 })
