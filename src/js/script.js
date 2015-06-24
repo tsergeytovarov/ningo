217541156e41855a79550247ebf94cb6f34851e8
tsergeytovarov <tovarov.piter@gmail.com>
2015-06-24 19:09:58 +0400

    появляющийся хедер

--- src/js/script.js
+++ src/js/script.js
@@ -20,4 +20,16 @@ $(function(){
     $(".main-header__left--basic").addClass("main-header__left--open");
     $(".main-header__left--find").removeClass("main-header__left--open");
   })
+
+  // появление хедера
+  $(window).on("scroll", function(){
+    var header = $('.js-main-header').offset().top;
+    var input = $(".index-screen__input").offset().top;
+
+    if( header > input){
+      $('.js-main-header').removeClass('main-header--hidden');
+    } else {
+      $('.js-main-header').addClass('main-header--hidden');
+    }
+  })
 })
